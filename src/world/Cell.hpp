1c2e66980a7510e598a166452549a5774650d216
ColeMcGregor <gc28mcgr@siena.edu>
2025-08-21 22:26:06 -0400

    Feat(Cell): added in flags to show whether a given cell has walls currently active.

--- src/world/Cell.hpp
+++ src/world/Cell.hpp
@@ -13,6 +13,12 @@ namespace minotaur::world {
 struct Cell {
     int x = 0;                       // x coordinate
     int y = 0;                       // y coordinate
+    // flags to reprensent the state of each wall of the cell, false means the wall is down
+    //these are changeable during the game to simulate the changing of the maze
+    bool north = false;
+    bool south = false;
+    bool east = false;
+    bool west = false;
     //operator overload to allow comparison of cells
     constexpr bool operator==(const Cell& o) const { return x == o.x && y == o.y; }
 };
