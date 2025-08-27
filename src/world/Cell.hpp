a77b3cea1961a91380a0a557769642baef616811
ColeMcGregor <gc28mcgr@siena.edu>
2025-08-27 17:26:46 -0400

    Feat(cell): added in neighbor struct, array, and neeigbor count. useful for algorithms

--- src/world/Cell.hpp
+++ src/world/Cell.hpp
@@ -10,6 +10,21 @@ namespace minotaur {
  * @copyright Copyright (c) 2025 Cole McGregor
  * @note This is a simple struct that represents a cell in the world
  */
+
+ /**
+  * @brief A neighbor of a cell
+  * @author @Cole_McGregor
+  * @note this is small struct to hold coordinates of any cell neighbors
+  */
+struct Neighbor
+{
+    int x;
+    int y;
+    //direction of the neighbor relative to the cell (northerly neighbor is 0, etc)
+    int direction;
+};
+
+
 struct Cell {
     int x = 0;                       // x coordinate
     int y = 0;                       // y coordinate
@@ -19,6 +34,12 @@ struct Cell {
     bool south = false;
     bool east = false;
     bool west = false;
+
+    //list of neighbors
+    Neighbor nbr[4];   //to cache the neighbors of the cell, makes math easier
+    //count of neighbors, will hold how many neighbors are present
+    int      nbrCount; //to be used for some algorithms
+    
     //operator overload to allow comparison of cells
     constexpr bool operator==(const Cell& o) const { return x == o.x && y == o.y; }
 };
